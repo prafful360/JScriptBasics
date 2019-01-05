6dfb427ec5eb0bfd0d995a34ec1f30f6adf36577
PRAFFUL RANJAN <40630844+prafful360@users.noreply.github.com>
2019-01-05 22:49:04 +0530

    Create Script.js

---
+++ Condition/Script.js
@@ -0,0 +1,16 @@
+//Check 2 number in which one is negative and other one is positive
+function positive_negative(x, y)
+{
+  if ((x < 0 && y > 0) || x > 0 && y < 0) 
+  {
+    return true;
+  }
+  else 
+  {
+    return false;
+  }
+}
+console.log(positive_negative(2, 2));
+console.log(positive_negative(-2, 2));
+console.log(positive_negative(2, -2));
+console.log(positive_negative(-2, -2));
