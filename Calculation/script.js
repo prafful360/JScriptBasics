fdf345d25b7d520342fe14056f6ca60976c38192
PRAFFUL RANJAN <40630844+prafful360@users.noreply.github.com>
2019-01-05 22:46:53 +0530

    Create script.js

---
+++ Calculation/script.js
@@ -0,0 +1,28 @@
+// function multiplyBy()
+// {
+//         num1 = document.getElementById("firstNumber").value;
+//         num2 = document.getElementById("secondNumber").value;
+//         document.getElementById("result").innerHTML = num1 * num2;
+// }
+
+// function divideBy() 
+// { 
+//         num1 = document.getElementById("firstNumber").value;
+//         num2 = document.getElementById("secondNumber").value;
+// document.getElementById("result").innerHTML = num1 / num2;
+// }
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+        console.log(num1*num2);
+        
+}
+
+function divideBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 / num2;
+}
